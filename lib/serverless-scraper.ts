9fe0f0f5282e88bb2ca6f27862a3cbbd85eed72c
FlightPath Deploy <deploy@flightpath.solutions>
2026-03-24 22:51:44 +0800

    Add 100+ routes, 50+ airlines, expanded hub network

--- lib/serverless-scraper.ts
+++ lib/serverless-scraper.ts
@@ -179,6 +179,649 @@ const AIRLINE_ROUTES: Record<string, {
     basePrice: 620,
     duration: { min: 760, max: 800 } // 12h40m - 13h20m
   },
+  // ===== EUROPE EXPANSION =====
+  'LHR-MAD': { 
+    airlines: ['BA', 'IB', 'U2'], 
+    basePrice: 85,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  'LHR-BCN': { 
+    airlines: ['BA', 'VY', 'U2'], 
+    basePrice: 75,
+    duration: { min: 125, max: 140 } // 2h05m - 2h20m
+  },
+  'LHR-FCO': { 
+    airlines: ['BA', 'AZ', 'U2'], 
+    basePrice: 95,
+    duration: { min: 150, max: 165 } // 2h30m - 2h45m
+  },
+  'LHR-MXP': { 
+    airlines: ['BA', 'AZ', 'U2'], 
+    basePrice: 90,
+    duration: { min: 130, max: 145 } // 2h10m - 2h25m
+  },
+  'LHR-ZUR': { 
+    airlines: ['BA', 'LX'], 
+    basePrice: 110,
+    duration: { min: 105, max: 120 } // 1h45m - 2h
+  },
+  'LHR-GVA': { 
+    airlines: ['BA', 'LX', 'U2'], 
+    basePrice: 100,
+    duration: { min: 100, max: 115 } // 1h40m - 1h55m
+  },
+  'LHR-VIE': { 
+    airlines: ['BA', 'OS'], 
+    basePrice: 125,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  'LHR-CPH': { 
+    airlines: ['BA', 'SK'], 
+    basePrice: 95,
+    duration: { min: 115, max: 130 } // 1h55m - 2h10m
+  },
+  'LHR-OSL': { 
+    airlines: ['BA', 'DY', 'SK'], 
+    basePrice: 110,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  'LHR-ARN': { 
+    airlines: ['BA', 'SK', 'DY'], 
+    basePrice: 115,
+    duration: { min: 145, max: 160 } // 2h25m - 2h40m
+  },
+  'LHR-DUB': { 
+    airlines: ['BA', 'EI', 'FR'], 
+    basePrice: 65,
+    duration: { min: 75, max: 90 } // 1h15m - 1h30m
+  },
+  'LHR-PRG': { 
+    airlines: ['BA', 'OK'], 
+    basePrice: 115,
+    duration: { min: 125, max: 140 } // 2h05m - 2h20m
+  },
+  'LHR-WAW': { 
+    airlines: ['BA', 'LO'], 
+    basePrice: 130,
+    duration: { min: 155, max: 170 } // 2h35m - 2h50m
+  },
+  'LHR-BUD': { 
+    airlines: ['BA', 'FR', 'W6'], 
+    basePrice: 120,
+    duration: { min: 150, max: 165 } // 2h30m - 2h45m
+  },
+  'LHR-ATH': { 
+    airlines: ['BA', 'A3', 'U2'], 
+    basePrice: 165,
+    duration: { min: 225, max: 240 } // 3h45m - 4h
+  },
+  'LHR-IST': { 
+    airlines: ['BA', 'TK'], 
+    basePrice: 185,
+    duration: { min: 230, max: 250 } // 3h50m - 4h10m
+  },
+  'LHR-BCN': { 
+    airlines: ['BA', 'VY', 'U2'], 
+    basePrice: 80,
+    duration: { min: 125, max: 140 } // 2h05m - 2h20m
+  },
+  'LHR-LIS': { 
+    airlines: ['BA', 'TP', 'U2'], 
+    basePrice: 95,
+    duration: { min: 155, max: 170 } // 2h35m - 2h50m
+  },
+  'LHR-HEL': { 
+    airlines: ['BA', 'AY'], 
+    basePrice: 140,
+    duration: { min: 175, max: 190 } // 2h55m - 3h10m
+  },
+  // ===== ASIA EXPANSION =====
+  'LHR-NRT': { 
+    airlines: ['BA', 'JL', 'NH'], 
+    basePrice: 680,
+    duration: { min: 710, max: 750 } // 11h50m - 12h30m
+  },
+  'LHR-ICN': { 
+    airlines: ['BA', 'KE', 'OZ'], 
+    basePrice: 720,
+    duration: { min: 680, max: 720 } // 11h20m - 12h
+  },
+  'LHR-PVG': { 
+    airlines: ['BA', 'MU', 'VS'], 
+    basePrice: 580,
+    duration: { min: 660, max: 700 } // 11h - 11h40m
+  },
+  'LHR-PEK': { 
+    airlines: ['BA', 'CA', 'CZ'], 
+    basePrice: 590,
+    duration: { min: 600, max: 640 } // 10h - 10h40m
+  },
+  'LHR-HND': { 
+    airlines: ['BA', 'JL', 'NH'], 
+    basePrice: 700,
+    duration: { min: 720, max: 760 } // 12h - 12h40m
+  },
+  'LHR-KUL': { 
+    airlines: ['BA', 'MH'], 
+    basePrice: 620,
+    duration: { min: 780, max: 820 } // 13h - 13h40m
+  },
+  'LHR-MNL': { 
+    airlines: ['BA', 'PR'], 
+    basePrice: 750,
+    duration: { min: 780, max: 840 } // 13h - 14h
+  },
+  'LHR-DEL': { 
+    airlines: ['BA', 'AI', 'VS'], 
+    basePrice: 520,
+    duration: { min: 510, max: 540 } // 8h30m - 9h
+  },
+  'LHR-BOM': { 
+    airlines: ['BA', 'AI', 'VS'], 
+    basePrice: 540,
+    duration: { min: 540, max: 570 } // 9h - 9h30m
+  },
+  'LHR-CMB': { 
+    airlines: ['BA', 'UL'], 
+    basePrice: 680,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'LHR-CGK': { 
+    airlines: ['BA', 'GA', 'SQ'], 
+    basePrice: 720,
+    duration: { min: 840, max: 900 } // 14h - 15h
+  },
+  // ===== MIDDLE EAST & AFRICA =====
+  'LHR-CAI': { 
+    airlines: ['BA', 'MS'], 
+    basePrice: 380,
+    duration: { min: 290, max: 310 } // 4h50m - 5h10m
+  },
+  'LHR-JNB': { 
+    airlines: ['BA', 'SA', 'VS'], 
+    basePrice: 680,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'LHR-CPT': { 
+    airlines: ['BA', 'VS'], 
+    basePrice: 750,
+    duration: { min: 720, max: 750 } // 12h - 12h30m
+  },
+  'LHR-LOS': { 
+    airlines: ['BA', 'VS'], 
+    basePrice: 620,
+    duration: { min: 390, max: 420 } // 6h30m - 7h
+  },
+  'LHR-ADD': { 
+    airlines: ['BA', 'ET'], 
+    basePrice: 580,
+    duration: { min: 450, max: 480 } // 7h30m - 8h
+  },
+  'LHR-TLV': { 
+    airlines: ['BA', 'LY', 'VS'], 
+    basePrice: 320,
+    duration: { min: 300, max: 320 } // 5h - 5h20m
+  },
+  'LHR-AUH': { 
+    airlines: ['BA', 'EY'], 
+    basePrice: 400,
+    duration: { min: 420, max: 440 } // 7h - 7h20m
+  },
+  'LHR-KWI': { 
+    airlines: ['BA', 'KU'], 
+    basePrice: 450,
+    duration: { min: 360, max: 390 } // 6h - 6h30m
+  },
+  // ===== AMERICAS EXPANSION =====
+  'LHR-YVR': { 
+    airlines: ['BA', 'VS', 'AC'], 
+    basePrice: 580,
+    duration: { min: 540, max: 570 } // 9h - 9h30m
+  },
+  'LHR-YYZ': { 
+    airlines: ['BA', 'VS', 'AC', 'AA'], 
+    basePrice: 480,
+    duration: { min: 480, max: 510 } // 8h - 8h30m
+  },
+  'LHR-DFW': { 
+    airlines: ['BA', 'AA'], 
+    basePrice: 620,
+    duration: { min: 600, max: 630 } // 10h - 10h30m
+  },
+  'LHR-SEA': { 
+    airlines: ['BA', 'AA'], 
+    basePrice: 560,
+    duration: { min: 570, max: 600 } // 9h30m - 10h
+  },
+  'LHR-DEN': { 
+    airlines: ['BA'], 
+    basePrice: 640,
+    duration: { min: 600, max: 630 } // 10h - 10h30m
+  },
+  'LHR-MEX': { 
+    airlines: ['BA', 'VS'], 
+    basePrice: 780,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'LHR-GRU': { 
+    airlines: ['BA', 'LA'], 
+    basePrice: 820,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'LHR-EZE': { 
+    airlines: ['BA'], 
+    basePrice: 950,
+    duration: { min: 780, max: 810 } // 13h - 13h30m
+  },
+  'LHR-SCL': { 
+    airlines: ['BA', 'LA'], 
+    basePrice: 880,
+    duration: { min: 840, max: 870 } // 14h - 14h30m
+  },
+  // ===== US DOMESTIC =====
+  'JFK-LAX': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 280,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  'JFK-SFO': { 
+    airlines: ['AA', 'DL', 'B6', 'UA'], 
+    basePrice: 320,
+    duration: { min: 360, max: 390 } // 6h - 6h30m
+  },
+  'JFK-MIA': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 180,
+    duration: { min: 180, max: 195 } // 3h - 3h15m
+  },
+  'JFK-ORD': { 
+    airlines: ['AA', 'DL', 'UA', 'B6'], 
+    basePrice: 150,
+    duration: { min: 150, max: 165 } // 2h30m - 2h45m
+  },
+  'JFK-LAS': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 240,
+    duration: { min: 330, max: 345 } // 5h30m - 5h45m
+  },
+  'JFK-DEN': { 
+    airlines: ['UA', 'DL', 'B6'], 
+    basePrice: 220,
+    duration: { min: 270, max: 285 } // 4h30m - 4h45m
+  },
+  'JFK-PHX': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 260,
+    duration: { min: 300, max: 315 } // 5h - 5h15m
+  },
+  'JFK-SEA': { 
+    airlines: ['AA', 'DL', 'AS', 'B6'], 
+    basePrice: 280,
+    duration: { min: 360, max: 375 } // 6h - 6h15m
+  },
+  'JFK-DFW': { 
+    airlines: ['AA', 'DL'], 
+    basePrice: 200,
+    duration: { min: 210, max: 225 } // 3h30m - 3h45m
+  },
+  'JFK-ATL': { 
+    airlines: ['DL', 'B6'], 
+    basePrice: 160,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  // ===== MORE EUROPEAN ROUTES =====
+  'CDG-FCO': { 
+    airlines: ['AF', 'AZ', 'U2', 'VY'], 
+    basePrice: 85,
+    duration: { min: 105, max: 120 } // 1h45m - 2h
+  },
+  'CDG-MAD': { 
+    airlines: ['AF', 'IB', 'UX', 'U2'], 
+    basePrice: 95,
+    duration: { min: 125, max: 140 } // 2h05m - 2h20m
+  },
+  'CDG-BCN': { 
+    airlines: ['AF', 'VY', 'U2'], 
+    basePrice: 90,
+    duration: { min: 95, max: 110 } // 1h35m - 1h50m
+  },
+  'CDG-FRA': { 
+    airlines: ['AF', 'LH'], 
+    basePrice: 110,
+    duration: { min: 80, max: 95 } // 1h20m - 1h35m
+  },
+  'CDG-AMS': { 
+    airlines: ['AF', 'KL'], 
+    basePrice: 95,
+    duration: { min: 85, max: 100 } // 1h25m - 1h40m
+  },
+  'CDG-ZUR': { 
+    airlines: ['AF', 'LX'], 
+    basePrice: 105,
+    duration: { min: 65, max: 80 } // 1h05m - 1h20m
+  },
+  'CDG-VIE': { 
+    airlines: ['AF', 'OS'], 
+    basePrice: 125,
+    duration: { min: 115, max: 130 } // 1h55m - 2h10m
+  },
+  'CDG-IST': { 
+    airlines: ['AF', 'TK'], 
+    basePrice: 195,
+    duration: { min: 195, max: 215 } // 3h15m - 3h35m
+  },
+  'CDG-ATH': { 
+    airlines: ['AF', 'A3'], 
+    basePrice: 175,
+    duration: { min: 175, max: 195 } // 2h55m - 3h15m
+  },
+  // ===== ASIA-ASIA ROUTES =====
+  'SIN-BKK': { 
+    airlines: ['SQ', 'TG', 'TR'], 
+    basePrice: 120,
+    duration: { min: 140, max: 155 } // 2h20m - 2h35m
+  },
+  'SIN-HKG': { 
+    airlines: ['SQ', 'CX', 'TR'], 
+    basePrice: 180,
+    duration: { min: 230, max: 250 } // 3h50m - 4h10m
+  },
+  'SIN-NRT': { 
+    airlines: ['SQ', 'JL', 'NH', 'TR'], 
+    basePrice: 420,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'SIN-ICN': { 
+    airlines: ['SQ', 'KE', 'OZ', 'TR'], 
+    basePrice: 380,
+    duration: { min: 390, max: 420 } // 6h30m - 7h
+  },
+  'SIN-PVG': { 
+    airlines: ['SQ', 'MU', 'TR'], 
+    basePrice: 320,
+    duration: { min: 300, max: 330 } // 5h - 5h30m
+  },
+  'SIN-DXB': { 
+    airlines: ['SQ', 'EK'], 
+    basePrice: 380,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'SIN-DEL': { 
+    airlines: ['SQ', 'AI', 'TR'], 
+    basePrice: 340,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  'SIN-SYD': { 
+    airlines: ['SQ', 'QF'], 
+    basePrice: 480,
+    duration: { min: 450, max: 480 } // 7h30m - 8h
+  },
+  'SIN-MEL': { 
+    airlines: ['SQ', 'QF'], 
+    basePrice: 520,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'BKK-HKG': { 
+    airlines: ['TG', 'CX', 'HX'], 
+    basePrice: 160,
+    duration: { min: 165, max: 185 } // 2h45m - 3h05m
+  },
+  'BKK-NRT': { 
+    airlines: ['TG', 'JL', 'NH'], 
+    basePrice: 380,
+    duration: { min: 360, max: 390 } // 6h - 6h30m
+  },
+  'BKK-ICN': { 
+    airlines: ['TG', 'KE', 'OZ'], 
+    basePrice: 360,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  'DXB-BKK': { 
+    airlines: ['EK', 'TG'], 
+    basePrice: 280,
+    duration: { min: 360, max: 390 } // 6h - 6h30m
+  },
+  'DXB-SIN': { 
+    airlines: ['EK', 'SQ'], 
+    basePrice: 320,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  // ===== AUSTRALIA ROUTES =====
+  'SYD-LAX': { 
+    airlines: ['QF', 'AA', 'DL'], 
+    basePrice: 820,
+    duration: { min: 780, max: 810 } // 13h - 13h30m
+  },
+  'SYD-SFO': { 
+    airlines: ['QF', 'UA'], 
+    basePrice: 850,
+    duration: { min: 840, max: 870 } // 14h - 14h30m
+  },
+  'SYD-HKG': { 
+    airlines: ['QF', 'CX'], 
+    basePrice: 580,
+    duration: { min: 540, max: 570 } // 9h - 9h30m
+  },
+  'SYD-NRT': { 
+    airlines: ['QF', 'JL', 'NH'], 
+    basePrice: 620,
+    duration: { min: 570, max: 600 } // 9h30m - 10h
+  },
+  'SYD-SIN': { 
+    airlines: ['QF', 'SQ'], 
+    basePrice: 480,
+    duration: { min: 480, max: 510 } // 8h - 8h30m
+  },
+  'MEL-LAX': { 
+    airlines: ['QF', 'AA', 'UA'], 
+    basePrice: 850,
+    duration: { min: 840, max: 870 } // 14h - 14h30m
+  },
+  'MEL-SIN': { 
+    airlines: ['QF', 'SQ', 'TR'], 
+    basePrice: 520,
+    duration: { min: 450, max: 480 } // 7h30m - 8h
+  },
+  // ===== REVERSAL ROUTES (for return flights) =====
+  'JFK-LAX': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 280,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  'LAX-JFK': { 
+    airlines: ['AA', 'DL', 'B6', 'UA'], 
+    basePrice: 290,
+    duration: { min: 300, max: 330 } // 5h - 5h30m
+  },
+  'SFO-JFK': { 
+    airlines: ['AA', 'DL', 'B6', 'UA'], 
+    basePrice: 310,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  'MIA-JFK': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 170,
+    duration: { min: 165, max: 180 } // 2h45m - 3h
+  },
+  'ORD-JFK': { 
+    airlines: ['AA', 'DL', 'UA', 'B6'], 
+    basePrice: 140,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  'BOS-JFK': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 95,
+    duration: { min: 75, max: 90 } // 1h15m - 1h30m
+  },
+  'ATL-JFK': { 
+    airlines: ['DL', 'B6'], 
+    basePrice: 150,
+    duration: { min: 120, max: 135 } // 2h - 2h15m
+  },
+  'DFW-JFK': { 
+    airlines: ['AA', 'DL'], 
+    basePrice: 190,
+    duration: { min: 195, max: 210 } // 3h15m - 3h30m
+  },
+  'SEA-JFK': { 
+    airlines: ['AA', 'DL', 'AS', 'B6'], 
+    basePrice: 270,
+    duration: { min: 300, max: 315 } // 5h - 5h15m
+  },
+  'DEN-JFK': { 
+    airlines: ['UA', 'DL', 'B6'], 
+    basePrice: 210,
+    duration: { min: 240, max: 255 } // 4h - 4h15m
+  },
+  'LAS-JFK': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 230,
+    duration: { min: 270, max: 285 } // 4h30m - 4h45m
+  },
+  'PHX-JFK': { 
+    airlines: ['AA', 'DL', 'B6'], 
+    basePrice: 250,
+    duration: { min: 240, max: 255 } // 4h - 4h15m
+  },
+  // ===== MORE EUROPE REVERSALS =====
+  'CDG-LHR': { 
+    airlines: ['AF', 'BA', 'U2'], 
+    basePrice: 115,
+    duration: { min: 75, max: 90 } // 1h15m - 1h30m
+  },
+  'AMS-LHR': { 
+    airlines: ['KL', 'BA', 'U2'], 
+    basePrice: 100,
+    duration: { min: 75, max: 90 } // 1h15m - 1h30m
+  },
+  'FRA-LHR': { 
+    airlines: ['LH', 'BA'], 
+    basePrice: 135,
+    duration: { min: 100, max: 115 } // 1h40m - 1h55m
+  },
+  'MAD-LHR': { 
+    airlines: ['IB', 'BA', 'U2'], 
+    basePrice: 90,
+    duration: { min: 135, max: 150 } // 2h15m - 2h30m
+  },
+  'BCN-LHR': { 
+    airlines: ['VY', 'BA', 'U2'], 
+    basePrice: 80,
+    duration: { min: 125, max: 140 } // 2h05m - 2h20m
+  },
+  'FCO-LHR': { 
+    airlines: ['AZ', 'BA', 'U2'], 
+    basePrice: 100,
+    duration: { min: 150, max: 165 } // 2h30m - 2h45m
+  },
+  'ZUR-LHR': { 
+    airlines: ['LX', 'BA'], 
+    basePrice: 115,
+    duration: { min: 105, max: 120 } // 1h45m - 2h
+  },
+  'DUB-LHR': { 
+    airlines: ['EI', 'BA', 'FR'], 
+    basePrice: 70,
+    duration: { min: 75, max: 90 } // 1h15m - 1h30m
+  },
+  // ===== CANADA ROUTES =====
+  'YYZ-LHR': { 
+    airlines: ['AC', 'BA', 'VS'], 
+    basePrice: 470,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'YVR-LHR': { 
+    airlines: ['AC', 'BA'], 
+    basePrice: 570,
+    duration: { min: 540, max: 570 } // 9h - 9h30m
+  },
+  'YYZ-JFK': { 
+    airlines: ['AC', 'AA', 'DL'], 
+    basePrice: 220,
+    duration: { min: 90, max: 105 } // 1h30m - 1h45m
+  },
+  'YVR-JFK': { 
+    airlines: ['AC', 'AS'], 
+    basePrice: 320,
+    duration: { min: 330, max: 360 } // 5h30m - 6h
+  },
+  // ===== MEXICO / LATIN AMERICA =====
+  'MEX-JFK': { 
+    airlines: ['AM', 'AA', 'DL'], 
+    basePrice: 280,
+    duration: { min: 300, max: 330 } // 5h - 5h30m
+  },
+  'MEX-LHR': { 
+    airlines: ['BA', 'VS'], 
+    basePrice: 790,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'GRU-JFK': { 
+    airlines: ['AA', 'DL', 'LA'], 
+    basePrice: 580,
+    duration: { min: 570, max: 600 } // 9h30m - 10h
+  },
+  'GRU-LHR': { 
+    airlines: ['BA', 'LA'], 
+    basePrice: 830,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'EZE-JFK': { 
+    airlines: ['AA', 'AR'], 
+    basePrice: 720,
+    duration: { min: 630, max: 660 } // 10h30m - 11h
+  },
+  'SCL-JFK': { 
+    airlines: ['AA', 'LA'], 
+    basePrice: 680,
+    duration: { min: 600, max: 630 } // 10h - 10h30m
+  },
+  // ===== MIDDLE EAST EXPANSION =====
+  'DXB-JFK': { 
+    airlines: ['EK'], 
+    basePrice: 780,
+    duration: { min: 840, max: 870 } // 14h - 14h30m
+  },
+  'DXB-LAX': { 
+    airlines: ['EK'], 
+    basePrice: 820,
+    duration: { min: 960, max: 1000 } // 16h - 16h40m
+  },
+  'DXB-SYD': { 
+    airlines: ['EK'], 
+    basePrice: 920,
+    duration: { min: 840, max: 870 } // 14h - 14h30m
+  },
+  'DXB-LHR': { 
+    airlines: ['EK', 'BA', 'QF'], 
+    basePrice: 390,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'DOH-JFK': { 
+    airlines: ['QR'], 
+    basePrice: 750,
+    duration: { min: 780, max: 810 } // 13h - 13h30m
+  },
+  'DOH-LHR': { 
+    airlines: ['QR', 'BA'], 
+    basePrice: 430,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'AUH-LHR': { 
+    airlines: ['EY', 'BA'], 
+    basePrice: 410,
+    duration: { min: 420, max: 450 } // 7h - 7h30m
+  },
+  'IST-JFK': { 
+    airlines: ['TK'], 
+    basePrice: 480,
+    duration: { min: 660, max: 690 } // 11h - 11h30m
+  },
+  'IST-LHR': { 
+    airlines: ['TK', 'BA'], 
+    basePrice: 195,
+    duration: { min: 230, max: 250 } // 3h50m - 4h10m
+  },
 };
 
 const AIRLINE_NAMES: Record<string, string> = {
@@ -203,16 +846,66 @@ const AIRLINE_NAMES: Record<string, string> = {
   'NZ': 'Air New Zealand',
   'EV': 'Evelop Airlines',
   'U2': 'easyJet',
+  // Additional airlines
+  'AC': 'Air Canada',
+  'AY': 'Finnair',
+  'OS': 'Austrian Airlines',
+  'SK': 'SAS',
+  'DY': 'Norwegian',
+  'EI': 'Aer Lingus',
+  'TP': 'TAP Portugal',
+  'OK': 'Czech Airlines',
+  'LO': 'LOT Polish Airlines',
+  'W6': 'Wizz Air',
+  'FR': 'Ryanair',
+  'VY': 'Vueling',
+  'UX': 'Air Europa',
+  'LX': 'Swiss International',
+  'A3': 'Aegean Airlines',
+  'MS': 'EgyptAir',
+  'SA': 'South African Airways',
+  'ET': 'Ethiopian Airlines',
+  'LY': 'El Al',
+  'KU': 'Kuwait Airways',
+  'EY': 'Etihad Airways',
+  'KE': 'Korean Air',
+  'OZ': 'Asiana Airlines',
+  'MU': 'China Eastern',
+  'CZ': 'China Southern',
+  'CA': 'Air China',
+  'AI': 'Air India',
+  'MH': 'Malaysia Airlines',
+  'GA': 'Garuda Indonesia',
+  'PR': 'Philippine Airlines',
+  'UL': 'SriLankan Airlines',
+  'HX': 'Hong Kong Airlines',
+  'TR': 'Scoot',
+  'LA': 'LATAM',
+  'AM': 'Aeromexico',
+  'AR': 'Aerolineas Argentinas',
+  'B6': 'JetBlue',
+  'AS': 'Alaska Airlines',
+  'WS': 'WestJet',
+  'TS': 'Air Transat',
 };
 
 // Hub connections for split tickets
 const HUB_CONNECTIONS: Record<string, string[]> = {
-  'LHR': ['DXB', 'DOH', 'IST', 'CDG', 'FRA', 'AMS'],
-  'JFK': ['LHR', 'CDG', 'FRA', 'DXB', 'IST'],
-  'LAX': ['LHR', 'NRT', 'SYD'],
-  'CDG': ['DXB', 'JFK', 'LAX'],
-  'BKK': ['DXB', 'DOH', 'SIN', 'HKG'],
-  'SIN': ['DXB', 'LHR', 'SYD'],
+  'LHR': ['DXB', 'DOH', 'IST', 'CDG', 'FRA', 'AMS', 'SIN', 'HKG'],
+  'JFK': ['LHR', 'CDG', 'FRA', 'DXB', 'IST', 'DOH', 'SIN'],
+  'LAX': ['LHR', 'NRT', 'SYD', 'SIN', 'DXB'],
+  'CDG': ['DXB', 'JFK', 'LAX', 'SIN', 'HKG'],
+  'FRA': ['DXB', 'JFK', 'SIN', 'BKK'],
+  'AMS': ['DXB', 'JFK', 'SIN', 'BKK'],
+  'BKK': ['DXB', 'DOH', 'SIN', 'HKG', 'ICN'],
+  'SIN': ['DXB', 'LHR', 'SYD', 'MEL', 'HKG'],
+  'DXB': ['LHR', 'JFK', 'SIN', 'SYD', 'BKK'],
+  'DOH': ['LHR', 'JFK', 'SIN', 'BKK'],
+  'IST': ['LHR', 'JFK', 'SIN', 'BKK'],
+  'HKG': ['LHR', 'JFK', 'SIN', 'SYD'],
+  'NRT': ['LAX', 'SIN', 'BKK'],
+  'SYD': ['SIN', 'DXB', 'LAX', 'HKG'],
+  'MEL': ['SIN', 'DXB'],
 };
 
 /**
